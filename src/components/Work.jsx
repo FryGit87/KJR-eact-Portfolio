ac82fff7a5f34e836abf77839f31bcbc9b4d6400
Kym Reilly <kymreilly@hotmail.com>
2022-07-22 09:25:23 +0930

    set up work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+      <div>
+        <div>
+          <p>Work</p>
+          <p>Some of my previous work</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
